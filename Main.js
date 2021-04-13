92b1e1cab47bf405daa07f8d72876aebdd373917
Takashi@Mac <hamada@gift10.co.jp>
2021-04-13 16:57:09 +0900

    新規行の追加

--- Main.js
+++ Main.js
@@ -1,16 +1,21 @@
-function myFunction() {
-  
-}
-
 function doGet(e) {
     var ss = SpreadsheetApp.getActiveSpreadsheet();
     var sheet = ss.getSheetByName("Request");
     var targetRow = sheet.getLastRow() + 1;
     // --
+    var idx = targetRow - 1;
+    var stateList = ["Open", "Close"];
+    var kindList = ["not_set", "bonwae", "cut", "click", "pitch", "tool", "general"];
     var name = e.parameter.name;
+    // --
+    sheet.getRange(targetRow, 1).setValue(idx);
+    setRule(sheet.getRange(targetRow, 2), stateList);
+    setRule(sheet.getRange(targetRow, 3), kindList);
+    sheet.getRange(targetRow, 4).setValue(name);
+}
 
-    sheet.getRange(targetRow, 1).setValue(name);
-    
-    
-    
+function setRule(range, ruleList) {
+var rule = SpreadsheetApp.newDataValidation().requireValueInList(ruleList).build();
+range.setDataValidation(rule);
+range.setValue(ruleList[0]);
 }
